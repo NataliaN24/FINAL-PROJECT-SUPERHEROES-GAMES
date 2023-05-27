b101381399c4212e700bb49dab6ea87cf0304410
Natalia <115610226+NataliaN24@users.noreply.github.com>
2023-05-27 22:33:21 +0300

    Admin update

--- FINAL PROJECT SUPERHEROES GAMES/Admin.h
+++ FINAL PROJECT SUPERHEROES GAMES/Admin.h
@@ -1,5 +1,16 @@
 #pragma once
+#include<iostream>
+#include<fstream>
+#include<cstring>
+#include <sstream>
+
+#include"MyString.h" 
 class Admin
 {
+	MyString name;
+	MyString surname;
+	MyString email;
+	MyString password;
+	int numOfAdmins;
 };
 
